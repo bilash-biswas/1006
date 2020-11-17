3c758a02a5859b843c21fdcdf313fc87b2bfb4c5
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-17 15:17:13 +0600

    Update and rename Java to Java.java

--- Java.java
+++ Java.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
      double a,b,c,d;
      Scanner input = new Scanner(System.in);
      a = input.nextDouble();
      b = input.nextDouble();
      d = input.nextDouble();
      c = (a * 2 + b * 3 + d * 5) / 10;
-        System.out.printf("MEDIA = %.1f\n",c);
+     System.out.printf("MEDIA = %.1f\n", c);
     }
 }
