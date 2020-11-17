3950ad55ecfd1850086f483fe1ec3e85ea15cb0f
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-17 15:14:43 +0600

    Update and rename Ruby to Ruby.rb

--- Ruby.rb
+++ Ruby.rb
@@ -2,4 +2,4 @@ num1 = gets().chomp().to_f
 num2 = gets().chomp().to_f
 num3 = gets().chomp().to_f
 num = ((num1 * 2) + (num2 * 3) + (num3 * 5)) / 10
-printf("MEDIA = %.1f\n",num)
+printf("MEDIA = %.1f\n", num)
