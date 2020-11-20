d3fe3e553d2a6c7df1c7d9b4cacf52f57921d96f
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-21 00:27:03 +0600

    Create Swift.swift

---
+++ Swift.swift
@@ -0,0 +1,6 @@
+import Foundation
+var num1 = Double(readLine()!)!
+var num2 = Double(readLine()!)!
+var num3 = Double(readLine()!)!
+var num = (num1 * 2 + num2 * 3 + num3 * 5) / 10
+print(String(format:"MEDIA = %0.1f",num))
